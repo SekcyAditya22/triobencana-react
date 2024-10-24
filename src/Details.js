c01b4052b2342b57a2aa963e97bb1450f11ea9cd
SekcyAditya22 <147706295+SekcyAditya22@users.noreply.github.com>
2024-10-24 23:15:18 +0700

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,60 @@
+import logogradient from "./assets/logo_trio.png";
+import logo from "./assets/logo_trio.png";
+
+
+import profile1 from "./assets/profile_adit.png";
+import profile2 from "./assets/profile_muza.png";
+import profile3 from "./assets/profile_gibranz.png";
+
+
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+
+export const personalDetails = [
+  {
+    nim: "22.83.0885",
+    name: "Muhammad Aditya Madjid",
+    tagline: "Yogyakarta, Indonesia",
+    img: profile1,
+    about: `Seorang Pria yang suka bercerita bersama temannya, berbicara sopan, tidak suka mengumpat. Selain itu, saya jago jungler. `,
+    kelas: "Teknik Komputer - 02",
+  },
+  {
+    nim: "22.83.0883",
+    name: "Muzakir M Nur",
+    tagline: "Ternate, Maluku Utara",
+    img: profile2,
+    about: `Hai, saya Muza!
+Saya mahasiswa yang hobi desain grafis, suka musik, dan main game. Kreativitas adalah cara saya mengekspresikan diri, baik lewat visual maupun suara. Saat tidak sibuk dengan kuliah, saya sering menghabiskan waktu dengan mendesain atau bermain game.`,
+    kelas: "Teknik Komputer - 02",
+  },
+  {
+    nim: "22.83.0831",
+    name: "Gibran Hait Sami",
+    tagline: "Papua, Indonesia",
+    img: profile3,
+    about: `Kesukaan saya meminta Avanza Putih pada Muzakir.`,
+    kelas: "Teknik Komputer - 02",
+  },
+];
+
+
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/SekcyAditya22",
+  github: "https://github.com/SekcyAditya22/react-porto-kelompok1-triobencana",
+};
+
+
+export const eduDetails = [
+  {
+    Position: "Mahasiswa Teknik Komputer",
+    Company: "Universitas Amikom Yogyakarta",
+    Location: "Condong Catur, DIY",
+    Type: "Reguler",
+    Duration: "2022",
+    className: "text-light-heading dark:text-light-heading",
+  },
+];
